525c0b3d40d19c10195c7ba8d7839a49af116877
Nishanth <nishanth1596@gmail.com>
2025-01-15 17:40:24 +0530

    included h1, main to make it more semantic

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,19 @@ import Testimonials from "../Testimonials/Testimonials";
 
 const App = () => {
   return (
-    <section className="mt-[5.13rem] max-w-[20.4rem] text-center lg:mt-[7.4rem] lg:max-w-[70rem] lg:text-left">
-      <ProductInfo />
-      <div className="mb-24 mt-12 grid grid-cols-1 gap-4 lg:mt-16 lg:grid-cols-3 lg:items-start lg:gap-[1.875rem]">
-        {testimonialData.map((item) => (
-          <Testimonials item={item} key={item.name} />
-        ))}
-      </div>
-    </section>
+    <main>
+      <header>
+        <h1 className="hidden">Social proof section</h1>
+      </header>
+      <section className="mt-[5.13rem] max-w-[20.4rem] text-center lg:mt-[7.4rem] lg:max-w-[70rem] lg:text-left">
+        <ProductInfo />
+        <div className="mb-24 mt-12 grid grid-cols-1 gap-4 lg:mt-16 lg:grid-cols-3 lg:items-start lg:gap-[1.875rem]">
+          {testimonialData.map((item) => (
+            <Testimonials item={item} key={item.name} />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 };
 
