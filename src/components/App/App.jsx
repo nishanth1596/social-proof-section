e814f93f960fdc7b6091b27087b41d55f6510be9
Nishanth <nishanth1596@gmail.com>
2025-01-18 11:45:32 +0530

    Accessibility fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,17 +6,17 @@ const App = () => {
   return (
     <>
       <header>
-        <h1 className="hidden">Social proof section</h1>
+        <h1 className="sr-only">Social proof section</h1>
       </header>
       <main>
-        <section className="mt-[5.13rem] max-w-[20.4rem] text-center lg:mt-[7.4rem] lg:max-w-[70rem] lg:text-left">
+        <div className="mt-[5.13rem] max-w-[20.4rem] text-center lg:mt-[7.4rem] lg:max-w-[70rem] lg:text-left">
           <ProductInfo />
           <div className="mb-24 mt-12 grid grid-cols-1 gap-4 lg:mt-16 lg:grid-cols-3 lg:items-start lg:gap-[1.875rem]">
             {testimonialData.map((item) => (
               <Testimonials item={item} key={item.name} />
             ))}
           </div>
-        </section>
+        </div>
       </main>{" "}
     </>
   );
